9659011d59cff824fe148334abf9daaafc119535
Diego Fernández <difernandez@uc.cl>
2020-08-18 17:52:47 -0400

    feat(product): add image

--- app/javascript/product.vue
+++ app/javascript/product.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="grid grid-cols-4 grid-rows-2 p-4">
-    <div class="col-span-3">
-      {{ product.name }}
-    </div>
-    <div class="col-span-3 text-sm text-gray-600">
-      {{ product.brand }}
-    </div>
-    <div class="self-center col-start-4 row-span-2 row-start-1 text-lg">
-      ${{ product.price }}
+  <div>
+    <img
+      :src="product.image_url"
+      class="h-64 m-auto"
+    >
+    <div class="grid grid-cols-4 grid-rows-2 p-4">
+      <div class="col-span-3">
+        {{ product.name }}
+      </div>
+      <div class="col-span-3 text-sm text-gray-600">
+        {{ product.brand }}
+      </div>
+      <div class="self-center col-start-4 row-span-2 row-start-1 text-lg">
+        ${{ product.price }}
+      </div>
     </div>
   </div>
 </template>
