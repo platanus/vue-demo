5951aa2ae43e26905950eab57a79196eaa7180a1
Diego Fernández <difernandez@uc.cl>
2020-08-18 16:24:08 -0400

    feat(): add basic product index

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ end
   mount Rswag::Api::Engine => '/api-docs'
   mount Rswag::Ui::Engine => '/api-docs'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :products, only: [:index]
 end
