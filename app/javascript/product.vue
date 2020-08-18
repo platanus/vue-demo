1ecfe0f925d8fe1659d0790560e011efe99fc5b9
Diego Fernández <difernandez@uc.cl>
2020-08-18 17:40:47 -0400

    feat(product): add product prop
    goes over props, vue dev tools and template interpolation

--- app/javascript/product.vue
+++ app/javascript/product.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>
-    Hello World!
+  <div class="grid grid-cols-4 grid-rows-2 p-4">
+    <div class="col-span-3">
+      {{ product.name }}
+    </div>
+    <div class="col-span-3 text-sm text-gray-600">
+      {{ product.brand }}
+    </div>
+    <div class="self-center col-start-4 row-span-2 row-start-1 text-lg">
+      ${{ product.price }}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    product: { type: Object, required: true },
+  },
 };
 </script>
