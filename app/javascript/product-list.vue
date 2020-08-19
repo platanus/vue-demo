75f61e191f77170c3ac039bb7003af6f9db2d685
Diego Fernández <difernandez@uc.cl>
2020-08-19 17:04:45 -0400

    feat(product): make products selectable
    Goes over methods, v-on, events, data, class bindings

--- app/javascript/product-list.vue
+++ app/javascript/product-list.vue
@@ -2,12 +2,13 @@
   <div>
     <div class="grid grid-cols-3 gap-4">
       <div
-        v-for="product in products"
+        v-for="product in normalizedProducts"
         :key="product.id"
         class="border"
       >
         <product
           :product="product"
+          @toggled-select="toggleSelect(product)"
         />
       </div>
     </div>
@@ -21,6 +22,24 @@ export default {
   components: { Product },
   props: {
     products: { type: Array, required: true },
+  },
+  data() {
+    const productsHash = {};
+    this.products.forEach((product) => {
+      productsHash[product.id] = { ...product, selected: false };
+    });
+
+    return {
+      normalizedProducts: productsHash,
+    };
+  },
+  methods: {
+    toggleSelect(product) {
+      this.normalizedProducts = {
+        ...this.normalizedProducts,
+        [product.id]: { ...product, selected: !product.selected },
+      };
+    },
   },
 };
 </script>
--- app/javascript/product.vue
+++ app/javascript/product.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-    <img
-      :src="product.image_url"
-      class="h-64 m-auto"
-    >
+    <div class="relative bg-black">
+      <button
+        class="absolute top-0 right-0 w-6 h-6 m-2 bg-white rounded-full focus:outline-none"
+        :class="{ 'bg-green-500': product.selected }"
+        @click="toggleSelected"
+      />
+      <img
+        :src="product.image_url"
+        class="h-64 m-auto"
+      >
+    </div>
     <div class="grid grid-cols-4 grid-rows-2 p-4">
       <div class="col-span-3">
         {{ product.name }}
@@ -22,6 +29,11 @@
 export default {
   props: {
     product: { type: Object, required: true },
+  },
+  methods: {
+    toggleSelected() {
+      this.$emit('toggled-select');
+    },
   },
 };
 </script>
