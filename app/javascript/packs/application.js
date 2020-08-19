239769ee4c599d814d2ace4a4247d6a56ba26317
Diego Fernández <difernandez@uc.cl>
2020-08-19 16:36:46 -0400

    feat(index): move list to vue component
    Goes over component composition

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,12 @@
 import Vue from 'vue/dist/vue.esm';
 import Product from '../product.vue';
+import ProductList from '../product-list.vue';
 import '../css/application.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     el: '#vue-app',
-    components: { Product },
+    components: { Product, ProductList },
   });
 
   return app;
---
+++ app/javascript/product-list.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="grid grid-cols-3 gap-4">
+      <div
+        v-for="product in products"
+        :key="product.id"
+        class="border"
+      >
+        <product
+          :product="product"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Product from './product';
+
+export default {
+  components: { Product },
+  props: {
+    products: { type: Array, required: true },
+  },
+};
+</script>
